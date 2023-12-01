40b68b34079a9d719478f5ad075f8ef3c4b9de2d
belyj_ai <belyj_ai@teleformis.ru>
2023-12-01 10:20:58 +0300

    renamed all files kebab declaration

--- reuse-patterns-using-capture-groups.ts
+++ reuse-patterns-using-capture-groups.ts
@@ -1,6 +1,6 @@
 /* Use capture groups in reRegex to match a string that consists of only the same number repeated exactly three times separated by single spaces.*/
 
 function isRepeatThreeStr (str: string): boolean {
-    let reRegex = /(\d+) \1 \1/;
+    let reRegex = /^(\d+) \1 \1$/;
     return  reRegex.test(str);
 }
