1d873b387c48729e704ab0b42e1937ed583f3d6a
belyj_ai <belyj_ai@teleformis.ru>
2023-12-14 00:54:15 +0300

    created function modify array-stored-object

---
+++ basic-data-structures/modify-array-stored-object.ts
@@ -0,0 +1,27 @@
+const user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
+    }
+};
+
+function addFriend(userObj, friend) {
+    const { friends } = user.data;
+    friends.push(friend);
+
+    // Only change code above this line
+    return friends;
+}
