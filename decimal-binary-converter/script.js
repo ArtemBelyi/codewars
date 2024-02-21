2715f8f74adeddc0f4053da1357bb539666ce86d
belyj_ai <belyj_ai@teleformis.ru>
2024-02-21 06:20:59 +0300

    added functionality into decimalToBinary function

--- decimal-binary-converter/script.js
+++ decimal-binary-converter/script.js
@@ -2,7 +2,13 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const decimalToBinary = (input) => {}
+const decimalToBinary = (input) => {
+    const inputs = [];
+    const quotients = [];
+    const remainders = [];
+
+    input = 0
+}
 
 const checkUserInput = () => {
     if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
